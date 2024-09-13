b94265e0e65aea00a99cee6b19b591ee4551a8a0
siyuanzhou <1848142281@qq.com>
2024-09-13 15:13:26 +0800

    Deep Copy & Shallow Copy in String initialization

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -24,7 +24,17 @@ public class Main {
 //  可变参数 类型...  定义，可变参数相当于数组类型，可变参数可以保证无法传入null，传入0个参数，接收到的实际值是一个空数组而不是null
 class Group {
     private String[] names;
+    String info;
 
+    public Group (String[] names, String info) {
+//        this.names = names; // 注意该方法是浅拷贝
+        this.names = new String[names.length];
+        for (int i = 0; i < names.length; i++) {
+            this.names[i] = names[i]; // 创造副本再赋值
+        }
+
+        this.info = info;
+    }
     public void setNames(String... names) {  //  相当于String[] names，区别在于 调用方需要自己先构造String[]
         //  main函数调用方 Group g = new Group(); g.setNames(new String[] {" "}); 并且调用方可以传入Null
         //  String... names 调用方可以直接g.setNames("xxx");
